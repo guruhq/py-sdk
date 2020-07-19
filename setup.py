6e836836f772b7c62e71685bdeb171f41035d5d9
Rob Miller <robert.ryan.miller@gmail.com>
2020-07-19 14:36:10 -0400

    update setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+import setuptools
+
+if __name__ == "__main__":
+  setuptools.setup(
+    name="guru",
+    version="1.0.0",
+    url="https://github.com/guruhq/py-sdk",
+    packages=["guru"],
+    python_requires=">=2.7"
+  )
