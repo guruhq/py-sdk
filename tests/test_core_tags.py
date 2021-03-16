d71a985e52be5a0ee27b588ee9218aeeaec3b9ea
Rob Miller <rmiller@getguru.com>
2021-03-16 16:08:03 -0400

    move tests for add_tag_to_card to test_core_tags

--- tests/test_core_tags.py
+++ tests/test_core_tags.py
@@ -242,7 +242,139 @@ class TestCore(unittest.TestCase):
       "url": "https://api.getguru.com/api/v1/teams/abcd/tagcategories"
     }])
 
+  
   @use_guru()
   @responses.activate
-  def test_make_card(self, g):
-    pass
+  def test_add_tag_to_card(self, g):
+    responses.add(responses.GET, "https://api.getguru.com/api/v1/cards/1234/extended", json={
+      "id": "1234"
+    })
+    responses.add(responses.GET, "https://api.getguru.com/api/v1/whoami", json={
+      "team": {
+        "id": "abcd"
+      }
+    })
+    responses.add(responses.GET, "https://api.getguru.com/api/v1/teams/abcd/tagcategories", json=[{
+      "tags": [{
+        "id": "abcd",
+        "value": "tag1"
+      }],
+      "id": "1234",
+      "name": "category"
+    }])
+    responses.add(responses.PUT, "https://api.getguru.com/api/v1/cards/1234/tags/abcd", json={})
+    responses.add(responses.PUT, "https://api.getguru.com/api/v1/cards/1234", json={})
+
+    card = g.get_card("1234")
+
+    # we call this twice but expect only one PUT call.
+    card.add_tag("tag1")
+    card.add_tag("tag1")
+
+    # calling add_tag() will save the card, but we want to make sure this
+    # saves the card and also includes tag1 as part of its data.
+    card.save()
+
+    self.assertEqual(get_calls(), [{
+      "method": "GET",
+      "url": "https://api.getguru.com/api/v1/cards/1234/extended"
+    }, {
+      "method": "GET",
+      "url": "https://api.getguru.com/api/v1/whoami"
+    }, {
+      "method": "GET",
+      "url": "https://api.getguru.com/api/v1/teams/abcd/tagcategories"
+    }, {
+      "method": "PUT",
+      "url": "https://api.getguru.com/api/v1/cards/1234/tags/abcd"
+    }, {
+      "method": "PUT",
+      "url": "https://api.getguru.com/api/v1/cards/1234",
+      "body": {
+        "cardType": "CARD",
+        "collection": None,
+        "content": "",
+        "id": "1234",
+        "preferredPhrase": "",
+        "shareStatus": "TEAM",
+        "tags": [{
+          "id": "abcd",
+          "value": "tag1",
+          "categoryName": None,
+          "categoryId": None
+        }],
+        "suppressVerification": True
+      }
+    }])
+
+  @use_guru()
+  @responses.activate
+  def test_add_new_tag_to_card(self, g):
+    responses.add(responses.GET, "https://api.getguru.com/api/v1/cards/1234/extended", json={
+      "id": "1234"
+    })
+    responses.add(responses.GET, "https://api.getguru.com/api/v1/whoami", json={
+      "team": {
+        "id": "abcd"
+      }
+    })
+    responses.add(responses.GET, "https://api.getguru.com/api/v1/teams/abcd/tagcategories", json=[{
+      "tags": [],
+      "name": "category",
+      "name": "Tags",
+      "id": "2222"
+    }])
+    responses.add(responses.POST, "https://api.getguru.com/api/v1/teams/abcd/tagcategories/tags", json={
+      "id": "abcd",
+      "value" : "new_tag",
+      "categoryName" : "Tags",
+      "categoryId" : "2222"
+    })
+    responses.add(responses.PUT, "https://api.getguru.com/api/v1/cards/1234/tags/abcd", json={})
+    responses.add(responses.PUT, "https://api.getguru.com/api/v1/cards/1234", json={})
+
+    card = g.get_card("1234")
+    card.add_tag("new_tag", create=True)
+
+    # calling add_tag() will save the card, but we want to make sure this
+    # saves the card and also includes 'new_tag' as part of its data.
+    card.save()
+
+    self.assertEqual(get_calls(), [{
+      "method": "GET",
+      "url": "https://api.getguru.com/api/v1/cards/1234/extended"
+    }, {
+      "method": "GET",
+      "url": "https://api.getguru.com/api/v1/whoami"
+    }, {
+      "method": "GET",
+      "url": "https://api.getguru.com/api/v1/teams/abcd/tagcategories"
+    }, {
+      "method": "POST",
+      "url": "https://api.getguru.com/api/v1/teams/abcd/tagcategories/tags",
+      "body": {
+        "categoryId": "2222",
+        "value": "new_tag"
+      }
+    }, {
+      "method": "PUT",
+      "url": "https://api.getguru.com/api/v1/cards/1234/tags/abcd"
+    }, {
+      "method": "PUT",
+      "url": "https://api.getguru.com/api/v1/cards/1234",
+      "body": {
+        "cardType": "CARD",
+        "collection": None,
+        "content": "",
+        "id": "1234",
+        "preferredPhrase": "",
+        "shareStatus": "TEAM",
+        "tags": [{
+          "id": "abcd",
+          "value": "new_tag",
+          "categoryName": "Tags",
+          "categoryId": "2222"
+        }],
+        "suppressVerification": True
+      }
+    }])
