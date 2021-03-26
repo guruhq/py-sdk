37ce5b019eac2952493940ecbae21ac13f93a648
John Chappelle <jchappelle@getguru.com>
2021-03-26 13:25:29 -0400

    remove () from open_comments

--- examples/comments.py
+++ examples/comments.py
@@ -5,6 +5,6 @@ g = guru.Guru()
 ## resolve all open comments
 for card in g.find_cards():
   open_comments = card.get_open_card_comments()
-  for comment in open_comments():
+  for comment in open_comments:
     if comment.is_before("2021-03-01"):
       comment.resolve()
