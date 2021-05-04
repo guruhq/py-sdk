aceb89b84a2fc520178211fc3b07fe10a7d3577d
John Chappelle <jchappelle@getguru.com>
2021-05-04 13:44:14 -0400

    add more docstrings, uncomment patch call

--- guru/find_and_replace.py
+++ guru/find_and_replace.py
@@ -7,6 +7,16 @@ from urllib.parse import quote, unquote
 from bs4 import BeautifulSoup
 
 def get_term_count(text, term):
+  """
+  Returns count of all occurrences of term in the text
+
+  Args:
+    text (str): text to search
+    term (str): term to find
+  
+  Returns: count (number) of all occurrences of term in the text
+
+  """
   doc = BeautifulSoup(text, "html.parser")
   lowered_doc = doc.text.lower()
   return lowered_doc.count(term.lower())
@@ -67,7 +77,7 @@ def replace_in_markdown_block(doc, term, replacement, replacement_case_sensitive
   """
   Replace text in markdown blocks (markdown content in data-ghq-card-content-markdown-content attribute)
 
-  Arguments:
+  Args:
     doc (BeautifulSoup): html document object
     term (str): term to replace
     replacement (str): replacement term
@@ -79,6 +89,18 @@ def replace_in_markdown_block(doc, term, replacement, replacement_case_sensitive
     markdown_div.attrs["data-ghq-card-content-markdown-content"] = quote(new_md)
 
 def replace_text_in_html(html, term, replacement, term_case_sensitive=False, replacement_case_sensitive=False):
+  """
+  Replaces term in html (string). Accounts for lowercase, uppercase, capitalized, and title-cased
+
+  Arguments:
+    html (BeautifulSoup or str): card's html (converted to BeautifulSoup object if not already)
+    term (str): term we want to replace,
+    replacement (str): replacement term,
+    term_case_sensitive (bool): boolean for searching for the specific term or case-insensitive (defaults to false ),
+    replacement_case_sensitive (bool): boolean for replacement term case-sensitivity (defaults to false)
+
+  Returns: html (str) with replacement
+  """
   doc = html if isinstance(html, BeautifulSoup) else BeautifulSoup(html, "html.parser")
   pattern = re.compile(r'%s' % term, re.IGNORECASE)
   text_nodes = doc.find_all(text=pattern) if not term_case_sensitive else doc.find_all(text=term)
@@ -126,6 +148,21 @@ def replace_text_in_card(card, term, replacement, replace_title=True, replacemen
       card = replace_text_in_html(card, term, replacement, term_case_sensitive=case_sensitive)
     return card
 class PreviewData:
+  """
+  Class that holds a card's preview data.
+
+  Args:
+    card (Card instance): instance of `Card`
+    term (str): term to replace
+    replacement (str): replacement term
+    orig_content (str): original card content
+    new_content (str): new content, with replaced term
+    
+  Properties:
+    replacement_term_count: returns a count of all occurrences of the replacement term in the card's text
+    original_term_count: returns a count of all occurrences of the original term in the card's text
+
+  """
   def __init__(self, card, term, replacement, orig_content, new_content):
     self.card_id = card.id
     self.title = card.title
@@ -142,6 +179,21 @@ class PreviewData:
   def original_term_count(self):
     return get_term_count(self.orig_content, self.term)
 class Preview:
+  """
+  Class that builds html tree and shows the find and replace preview in the browser.
+
+  Args:
+    content_list (list of Preview): List of preview data objects (Preview)
+    term (str): term to replace
+    replacement (str): replacement term
+    folder (str): destination folder for preview files
+    task_name (str): name to be included in preview file path ( i.e., /tmp/find_and_replace/new_content/new_1111.html )
+    
+  Methods:
+    make_html_tree: builds the original content/new content link in the HTML preview page
+    view_in_browser: generates preview HTML page and shows in browser
+
+  """
   def __init__(self, content_list, term, replacement, folder="/tmp/", task_name=None):
     self.content_list = content_list
     self.term = term
@@ -333,6 +385,51 @@ class Preview:
         webbrowser.open_new_tab("file://" + self.card_preview_path % self.task_name)
 
 class FindAndReplace:
+  """
+  Performs a find and replace over a given set of cards (all cards you can access, by default), and shows a preview of 
+  the changes in the browser.
+
+  ```
+  import guru
+  g = guru.Guru()
+
+  term_to_replace = "Test"
+  replace_with_term = "Quiz"
+  task_name = "test_preview"
+
+  find_and_replace = guru.FindAndReplace(g, term_to_replace, replace_with_term, replace_title=True, task_name=task_name)
+  find_and_replace.run()
+  ```
+  Args:
+      collection (str): Either a collection name or ID. If it's a name, it'll
+        return the first matching collection and the comparison is not case
+        sensitive.
+      cache (bool, optional): If we're looking up a collection by name we'll
+        fetch all collections and then look for a match in the results and this
+        flag tells us whether we should use the results from the previous
+        /collections API call or make a new call. Defaults to False.
+    
+    Returns:
+      Collection: An object representing the collection.
+  Args:
+    guru (Guru instance): instance of Guru module
+    term (str): term to replace
+    replacement (str): replacement term
+    term_case_sensitive (bool): boolean for searching for the specific term or case-insensitive (defaults to false ),
+    replacement_case_sensitive (bool): boolean for replacement term case-sensitivity (defaults to false)
+    replace_title (bool): boolean for replacing term in card title ( defaults to false )
+    collection (str): Either a collection name or ID. If it's a name, it'll
+        return the first matching collection and the comparison is not case
+        sensitive.
+    folder (str): destination folder for preview files
+    task_name (str): name to be included in preview file path ( i.e., /tmp/find_and_replace/new_content/new_1111.html )
+    
+  Methods:
+    run: Run a find and replace, with the parameters provided, and previews in the browser. 
+    By default, this does a dry run, and doesn't post the changes to the card, 
+    but setting `dry_run=False` will make a patch request, with the card changes.
+
+  """
   def __init__(self, guru, term, replacement, term_case_sensitive=False, replacement_case_sensitive=False, replace_title=False, collection=None, folder="/tmp/", task_name=None):
     self.guru = guru
     self.term = term
@@ -344,6 +441,12 @@ class FindAndReplace:
     self.cards = []
 
   def run(self, dry_run=True):
+    """
+    Performs a find and replace on the given cards, 
+    and previews in the browser. Defaults to a dry run, 
+    but setting `dry_run=False` will make a patch request, with the card changes.
+    
+    """
     for card in self.guru.find_cards(collection=self.collection):
       if card.has_text(self.term, case_sensitive=self.term_case_sensitive):
         
@@ -352,8 +455,7 @@ class FindAndReplace:
         card.content = replace_text_in_card(card, self.term, self.replacement, replace_title=self.replace_title, case_sensitive=self.term_case_sensitive)
         new_content =  card.content
         if not dry_run:
-          # card.patch()
-          print("PATCHING: ", original_content, new_content)
+          card.patch()
 
         self.cards.append(PreviewData(
           card=card,
