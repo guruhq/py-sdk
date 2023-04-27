6790fa80378767d9097a5015265da72f651c7c26
mark <mhornak@getguru.com>
2023-04-27 14:22:22 -0400

    update to add Folders to __init__.py

--- guru/__init__.py
+++ guru/__init__.py
@@ -1,53 +1,54 @@
 from guru.core import (
-  Guru,
-  # collection colors:
-  MAROON,
-  RED,
-  ORANGE,
-  AMBER,
-  SAPPHIRE,
-  CORNFLOWER,
-  TEAL,
-  GREEN,
-  MAGENTA,
-  DODGER_BLUE,
-  SALMON,
-  GREEN_APPLE,
-  # collection roles:
-  READ_ONLY,
-  AUTHOR,
-  COLLECTION_OWNER,
+    Guru,
+    # collection colors:
+    MAROON,
+    RED,
+    ORANGE,
+    AMBER,
+    SAPPHIRE,
+    CORNFLOWER,
+    TEAL,
+    GREEN,
+    MAGENTA,
+    DODGER_BLUE,
+    SALMON,
+    GREEN_APPLE,
+    # collection roles:
+    READ_ONLY,
+    AUTHOR,
+    COLLECTION_OWNER,
 
-  # verification states:
-  VERIFIED,
-  UNVERIFIED,
+    # verification states:
+    VERIFIED,
+    UNVERIFIED,
 )
 
 from guru.publish import (
-  Publisher
+    Publisher
 )
 
 # you might need these to check if an item on a board is
 # an instance of Section or Card.
 from guru.data_objects import (
-  Board,
-  BoardGroup,
-  Card,
-  Section
+    Board,
+    BoardGroup,
+    Card,
+    Folder,
+    Section
 )
 
 from guru.util import (
-  MAX_FILE_SIZE,
-  load_html,
-  http_get,
-  http_post,
-  download_file,
-  write_file,
-  read_file,
-  copy_file,
-  clear_dir,
-  format_timestamp,
-  compare_datetime_string,
-  save_json,
-  load_json
+    MAX_FILE_SIZE,
+    load_html,
+    http_get,
+    http_post,
+    download_file,
+    write_file,
+    read_file,
+    copy_file,
+    clear_dir,
+    format_timestamp,
+    compare_datetime_string,
+    save_json,
+    load_json
 )
