0294104b21f00137242be1576a5bc01e7e95da2d
markhornak <94062058+markhornak@users.noreply.github.com>
2023-05-02 13:06:16 -0400

    updated testing file

--- examples/folder_testing.py
+++ examples/folder_testing.py
@@ -6,3 +6,10 @@ email = "mhornak@getguru.com"
 token = "0ab65098-43df-42d6-9d22-bf4117f6b163"
 
 g = guru.Guru(email, token, qa=True)
+
+g.get_folder()
+
+for folder in g.get_folders(collection="786f6fc8-413b-418d-ba9b-fba974192401"):
+  print("Id: %s --> %s" % (folder.id, folder.title))
+
+folder = get_folder("iGqxqEgT/413-Testing")
