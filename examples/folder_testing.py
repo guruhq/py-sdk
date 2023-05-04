94c124678f0825fbf383dbeaa6ad1a4c250784f1
markhornak <94062058+markhornak@users.noreply.github.com>
2023-05-04 08:22:18 -0400

    update folder_testing.py to remove user/api token info

--- examples/folder_testing.py
+++ examples/folder_testing.py
@@ -8,8 +8,6 @@ email = "someperson@yourcompany.com"
 token = "yourapitokengoeshere" 
 """
 
-email = "mhornak@getguru.com"
-token = "0ab65098-43df-42d6-9d22-bf4117f6b163"
 
 g = guru.Guru(email, token, qa=True)
 
