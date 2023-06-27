759257636a38baf29ea585e2079407991496de8b
markhornak <94062058+markhornak@users.noreply.github.com>
2023-06-27 12:18:33 -0400

    bump release

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ import setuptools
 
 if __name__ == "__main__":
   setuptools.setup(
-    name="guru",
-    version="1.0.0",
-    url="https://github.com/guruhq/py-sdk",
-    packages=["guru"],
-    install_requires=[
-        "beautifulsoup4",
-        "markdown",
-        "python-dateutil",
-        "pytz",
-        "PyYAML",
-        "requests",
-    ],
-    python_requires=">=2.7"
+      name="guru",
+      version="1.1.0",
+      url="https://github.com/guruhq/py-sdk",
+      packages=["guru"],
+      install_requires=[
+          "beautifulsoup4",
+          "markdown",
+          "python-dateutil",
+          "pytz",
+          "PyYAML",
+          "requests",
+      ],
+      python_requires=">=2.7"
   )
